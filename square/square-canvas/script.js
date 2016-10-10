4a77d20a41587ba518c5fb826a20129728d54d1c
zmcdaniel <zmcdaniel@gmail.com>
2016-10-10 15:21:24 -0700

    Corrected colors

--- square/square-canvas/script.js
+++ square/square-canvas/script.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById('squares-canvas');
 var context = canvas.getContext('2d');
 
-context.fillStyle = "yellow";
+context.fillStyle = "#FEFF0C";
 context.fillRect(125, 125, 250, 250);
