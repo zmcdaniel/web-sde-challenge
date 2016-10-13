5fab64bbcb34f42205a7227ba32e3fa7fd9b74f6
zmcdaniel <zmcdaniel@gmail.com>
2016-10-12 18:10:48 -0700

    Percentages implemented and functioning

--- poll/js/app.js
+++ poll/js/app.js
@@ -5,12 +5,19 @@ app.controller('pollCtrl', ['$scope', function($scope) {
     $scope.maleCount = 0;
     $scope.femaleCount = 0;
 
+    $scope.malePercentage = 0;
+    $scope.femalePercentage = 0;
+
     $scope.incrementMale = function() {
         $scope.maleCount += 1;
+        $scope.malePercentage = Math.floor($scope.maleCount / ($scope.femaleCount + $scope.maleCount) * 100);
+        $scope.femalePercentage = Math.floor($scope.femaleCount / ($scope.femaleCount + $scope.maleCount) * 100);
     };
 
     $scope.incrementFemale = function() {
         $scope.femaleCount++;
+        $scope.malePercentage = Math.floor($scope.maleCount / ($scope.femaleCount + $scope.maleCount) * 100);
+        $scope.femalePercentage = Math.floor($scope.femaleCount / ($scope.femaleCount + $scope.maleCount) * 100);
     };
 
 
