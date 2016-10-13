82cc5cff616695a10f0c0246754af2e02364484b
zmcdaniel <zmcdaniel@gmail.com>
2016-10-12 18:08:50 -0700

    Functions to increment vote count

--- poll/js/app.js
+++ poll/js/app.js
@@ -5,4 +5,13 @@ app.controller('pollCtrl', ['$scope', function($scope) {
     $scope.maleCount = 0;
     $scope.femaleCount = 0;
 
+    $scope.incrementMale = function() {
+        $scope.maleCount += 1;
+    };
+
+    $scope.incrementFemale = function() {
+        $scope.femaleCount++;
+    };
+
+
 }]);
